68d15568c3f3e984d9ef6a237470d2d8eb0830b1
Dairush <webid@blinqx.com>
2012-09-16 01:14:56 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'e08ab47757e414b2be0427e8741898f95ecda33fa61cf2f388b57f68220d892fbb673ed697e345ebbb4147519906240501e7bf1021e921f2749903241ea573bf'
